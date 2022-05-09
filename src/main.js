5e2ed789b5a21a954e0c069289c7d490d764ea87
ASkeTizy <artemnevmerz@gmail.com>
2022-05-09 23:28:14 +0300

    add: virtual key-board

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import "./keyboard"
+import "./main.scss"
+
+// const fLine = ['/`', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12']
+// const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=']
+// const line1 = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '/\//']
+
+// document.addEventListener('DOMContentLoaded',()=>{
+//     const el = document.createElement("div")
+//     el.innerText = "1111111111"
+//     document.body.append(el)
+// })
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      compress: true,
+      port: 9000,
+      client: {
+        overlay: {
+          warnings: false
+        }
+      }
+    },
+  
+    module: {
+        rules: [
+          {
+            test: /\.s[ca]ss$/,
+            use: [
+              {loader: 'style-loader'},
+              {
+                loader: 'css-loader',
+                options: {
+                  url: false
+                }},
+              {loader: 'sass-loader'}
+            ]
+          }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin(),
+    //   new CopyPlugin({
+    //     patterns: [
+         
+    //     //   {
+    //     //     context: path.resolve(__dirname, "dist"),
+    //     //     from: path.resolve(__dirname, "src", "pages")
+    //     //   }
+    //     ]
+    //   })
+     ]
+ }
